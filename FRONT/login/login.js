76610b6ddfa805c42d5c87a7d368f1bea5b9ff2d
Mance04 <107369710+Mance04@users.noreply.github.com>
2023-05-16 19:32:14 +0200

    lalalala

--- FRONT/login/login.js
+++ FRONT/login/login.js
@@ -1,22 +1,65 @@
-
-
-
 const LINK = "http://localhost:8080";
 
 async function login(){
     const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const pass = document.getElementById('password').value;
 
-        let res=await axios.post(LINK+"/api/login",{
-            email:email,
-            password:password
-        });
-        console.log(res);
-        
-        if(res.data.uspesnost===true){
-            
-            location.href="../index.html"
-            console.log("1");
-            localStorage.setItem("id",res.data.id);
-        }
-}
+        document.getElementById("invalid-feedback1").innerHTML = "";
+        document.getElementById("invalid-feedback2").innerHTML = "";
+
+        if(email==="")
+            {
+                document.getElementById("invalid-feedback1").innerHTML="Niste uneli Email!!!"
+            }
+            else if(pass === "")
+            {
+                document.getElementById("invalid-feedback2").innerHTML="Niste uneli sifru!!!"
+            }
+            else if(!email.includes('@'))
+            {
+                document.getElementById("invalid-feedback1").innerHTML="Email nije validan!!!";
+            }
+            else if(pass.length<8)
+            {
+                document.getElementById("invalid-feedback2").innerHTML="Sifra ima manje od 8 karaktera!!!";
+                
+            }
+            else
+            {
+                let t = false;
+                let u = false;
+                let v = false;
+
+                for (let i = 0; i < pass.length; i++) {
+                    let element = pass.charCodeAt(i);
+
+                if (element >= 97 && element <= 122) {
+                    t = true;
+                } else if (element >= 65 && element <= 90) {
+                    u = true;
+                } else if (element >= 48 && element <= 57) {
+                    v = true;
+                    }
+                }
+            }
+            if (!t || !u || !v) {
+            document.getElementById("invalid-feedback2").innerHTML = "Šifra nije dovoljno sigurna";
+            }
+                else
+                {
+                    let res = (await axios.post(LINK+"/api/user",{
+                        sifra:pass,
+                        email:email
+                    })).data;
+                    console.log(res);
+                    if(res.uspesnost)
+                    {
+                        localStorage.setItem("id",res.saved_user._id)
+                        location.href="../index.html";
+                    }
+                    else
+                    {
+                        document.getElementById("invalid-feedback3").innerHTML=res.message;
+                    }
+                }
+    }
